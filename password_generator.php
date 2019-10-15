71a0365160f1c2201db2e12a78a85059f642a291
Alok Ranjan <33223353+Alokranjanvand@users.noreply.github.com>
2019-10-15 18:04:59 +0530

    Update password_generator.php

--- password_generator.php
+++ password_generator.php
@@ -1,1 +1,9 @@
+<?php
+$length = 10;
+$chars = array_merge(range(0,9), range('a','z'), range('A','Z'));
+shuffle($chars);
+$password = implode(array_slice($chars, 0, $length));
+echo $password;
+?>
+
 
