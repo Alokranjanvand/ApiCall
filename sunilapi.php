e8fd43020660d72991ccc97661515e46a0312d22
Alok Ranjan <33223353+Alokranjanvand@users.noreply.github.com>
2020-08-29 15:01:44 +0530

    Update sunilapi.php

--- sunilapi.php
+++ sunilapi.php
@@ -1,8 +1,10 @@
 <?php
 
 $curl = curl_init();
-$a=$_GET['a'];
-$b=$_GET['b'];
+//$a=$_GET['a'];
+//$b=$_GET['b'];
+$a="54e";
+$b="200310";
 
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://singapore.decipherinc.com/api/v1/surveys/selfserve/".$a."/".$b."/data?format=json",
@@ -21,4 +23,31 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 
 curl_close($curl);
-echo $response;
+$abc=array();
+$arr=json_decode($response,true);
+$abc=$arr[0];
+$data=array_keys($abc);
+$query = "DROP TABLE IF EXISTS `$b`; CREATE TABLE `$b` ( 
+`id` int(11) NOT NULL AUTO_INCREMENT, ";
+foreach($data as $key)
+{
+  $query .= "$key" . " " ."VARCHAR (255)," ;
+}
+$query.="PRIMARY KEY (`id`)";
+$query .= " ); ";
+$final_query=rtrim($query,',');
+echo $final_query;//output and check your query
+$data=implode(",",$data);
+//echo $data;
+$arr2=json_decode($response,true);
+echo"<pre>";
+//print_r($arr2);
+foreach($arr2 as $value)
+{
+	$key=array_keys($value);
+  $col=implode(",",$key);
+	$val=implode("','",$value);
+	 echo $sql="INSERT INTO `$b` ($col) VALUES ($val)";
+echo"<br><br><br><br><br>";
+}
+?>
