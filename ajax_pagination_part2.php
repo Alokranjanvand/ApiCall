823769d844832e9277f0a296bd8f65f7e7434d33
Alok Ranjan <33223353+Alokranjanvand@users.noreply.github.com>
2021-07-10 16:25:23 +0530

    Create ajax_pagination_part2.php

---
+++ ajax_pagination_part2.php
@@ -0,0 +1,142 @@
+<?php
+/*
+Author: Javed Ur Rehman
+Website: https://www.allphptricks.com
+*/
+?>
+<html>
+<head>
+<title>Create Simple Pagination Using PHP and MySQLi - AllPHPTricks.com</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div style="width:700px; margin:0 auto;">
+
+<h3>Demo Create Simple Pagination Using PHP and MySQLi</h3>
+<table class="table table-striped table-bordered">
+<thead>
+<tr>
+ <th scope="col" class="bg-primary" >Id</th>
+ <th scope="col" class="bg-primary">First Name</th>
+</tr>
+</thead>
+<tbody>
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "ehs@#$987";
+$dbname = "dairy_bank";
+$link1 = mysql_connect($dbhost,$dbuser,$dbpass) or mysql_error("Not able to connect with MySql Server");
+$link = mysql_select_db($dbname,$link1) or mysql_error("Not able to connect with Database");
+
+if (isset($_POST['page_no']) && $_POST['page_no']!="") 
+{
+	$page_no = $_POST['page_no'];
+}else
+{
+	$page_no = 1;
+}
+$limit = 1;
+$offset = ($page_no-1) * $limit;
+$previous_page = $page_no - 1;
+$next_page = $page_no + 1;
+$adjacents = "2"; 
+
+	$result_count = mysql_query("SELECT * FROM `dli_demand`");
+	$total_records = mysql_num_rows($result_count);
+    $total_no_of_pages = ceil($total_records / $limit);
+	$second_last = $total_no_of_pages - 1; // total page minus 1
+	echo $sql="SELECT * FROM `dli_demand` LIMIT $offset, $limit";
+    $result = mysql_query($sql);
+    while($row = mysql_fetch_array($result))
+	{
+		echo "<tr>
+			  <td>".$row['id']."</td>
+			  <td>".$row['order_type']."</td>
+		   	  </tr>";
+    }
+    ?>
+</tbody>
+</table>
+
+<div style='padding: 10px 20px 0px; border-top: dotted 1px #CCC;'>
+<strong>Page <?php echo $page_no." of ".$total_no_of_pages; ?></strong>
+</div>
+
+<ul class="pagination">
+	<?php // if($page_no > 1){ echo "<li><a href='?page_no=1'>First Page</a></li>"; } ?>
+    
+	<li <?php if($page_no <= 1){ echo "class='disabled'"; } ?>>
+	<a <?php if($page_no > 1){ echo "href='javascript:void(0)' id='$previous_page'";  } ?> id=''>Previous</a>
+	</li>
+       
+    <?php 
+	if ($total_no_of_pages <= 10){  	 
+		for ($counter = 1; $counter <= $total_no_of_pages; $counter++){
+			if ($counter == $page_no) {
+		   echo "<li class='active'><a>$counter</a></li>";	
+				}else{
+           echo "<li><a href='javascript:void(0)' id='$counter'>$counter</a></li>";
+				}
+        }
+	}
+	elseif($total_no_of_pages > 10){
+		
+	if($page_no <= 4) {			
+	 for ($counter = 1; $counter < 8; $counter++){		 
+			if ($counter == $page_no) {
+		   echo "<li class='active'><a>$counter</a></li>";	
+				}else{
+           echo "<li><a href='javascript:void(0)' id='$counter'>$counter</a></li>";
+				}
+        }
+		echo "<li><a>...</a></li>";
+		echo "<li><a href='javascript:void(0)' id='$second_last'>$second_last</a></li>";
+		echo "<li><a href='javascript:void(0)' id='$total_no_of_pages'>$total_no_of_pages</a></li>";
+		}
+
+	 elseif($page_no > 4 && $page_no < $total_no_of_pages - 4) {		 
+		echo "<li><a href='javascript:void(0)' id='1'>1</a></li>";
+		echo "<li><a href='javascript:void(0)' id='2'>2</a></li>";
+        echo "<li><a>...</a></li>";
+        for ($counter = $page_no - $adjacents; $counter <= $page_no + $adjacents; $counter++) {			
+           if ($counter == $page_no) {
+		   echo "<li class='active'><a>$counter</a></li>";	
+				}else{
+           echo "<li><a href='javascript:void(0)' id='$counter'>$counter</a></li>";
+				}                  
+       }
+       echo "<li><a>...</a></li>";
+	   echo "<li><a href='javascript:void(0)' id='$second_last'>$second_last</a></li>";
+	   echo "<li><a href='javascript:void(0)' id='$total_no_of_pages'>$total_no_of_pages</a></li>";      
+            }
+		
+		else {
+        echo "<li><a href='javascript:void(0)' id='1'>1</a></li>";
+		echo "<li><a href='javascript:void(0)' id='2'>2</a></li>";
+        echo "<li><a>...</a></li>";
+
+        for ($counter = $total_no_of_pages - 6; $counter <= $total_no_of_pages; $counter++) {
+          if ($counter == $page_no) {
+		   echo "<li class='active'><a>$counter</a></li>";	
+				}else{
+           echo "<li><a href='javascript:void(0)' id='$counter'>$counter</a></li>";
+				}                   
+                }
+            }
+	}
+?>
+    
+	<li <?php if($page_no >= $total_no_of_pages){ echo "class='disabled'"; } ?>>
+	<a <?php if($page_no < $total_no_of_pages) { echo "href='javascript:void(0)' id='$next_page'"; } ?>>Next</a>
+	</li>
+    <?php if($page_no < $total_no_of_pages){
+		echo "<li><a href='javascript:void(0)' id='$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+		} ?>
+</ul>
+</div>
+</body>
+</html>
